f1a4d203074039722402205bd513cbb424a229d8
kashishbehl0310 <kashishbehl36@gmail.com>
2018-08-12 16:19:14 +0530

    Single store page created

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -76,3 +76,14 @@ exports.updateStore = async(req, res) => {
     req.flash('success', `Successfully update ${store.name}. <a href="/stores/${store.slug}">View Store</a>`)
     res.redirect(`/stores/${store._id}/edit`)
 }
+
+exports.getStoreBySlug = async(req, res, next) => {
+    const store = await Store.findOne({ slug: req.params.slug})
+    if(!store){
+        return next()
+    }
+    res.render('store', {
+        store, 
+        title: store.name
+    })
+}
