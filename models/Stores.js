a0aefb960746fd5528dd6055c04647efec4b9bbb
kashishbehl0310 <kashishbehl36@gmail.com>
2018-08-12 14:43:57 +0530

    Uploading and resizing Images

--- models/Stores.js
+++ models/Stores.js
@@ -31,7 +31,8 @@ const storeSchema = new mongoose.Schema({
      type: String,
      required: 'Please provide an address'
    }
-  }
+  },
+  photo: String
 });
 
 storeSchema.pre('save', function(next) {
