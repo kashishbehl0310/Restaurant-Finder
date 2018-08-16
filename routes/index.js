6212392eb2ea6eef6744122594e0c7d88531df74
kashishbehl0310 <kashishbehl36@gmail.com>
2018-08-16 22:33:52 +0530

    User authentication

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController')
+const userController = require('../controllers/userController')
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
@@ -21,5 +22,7 @@ router.get('/store/:slug', storeController.getStoreBySlug)
 
 router.get('/tags', catchErrors(storeController.getStoreByTag ))
 router.get('/tags/:tag', catchErrors(storeController.getStoreByTag))
+
+router.get('/login', userController.loginForm)
 
 module.exports = router;
