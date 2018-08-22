15cc68a7b32ef8c66c949afb3532c7256998cbe8
kashishbehl0310 <kashishbehl36@gmail.com>
2018-08-22 11:19:18 +0530

    User specific content

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -45,6 +45,7 @@ exports.resize = async (req, res, next) => {
 }
 
 exports.createStore = async (req, res) => {
+    req.body.author = req.user._id
     const store = await (new Store(req.body)).save()
     req.flash('success', `Successfully created ${store.name}. Care to a leave a review`)
     res.redirect(`/store/${store.slug}`)
@@ -59,9 +60,16 @@ exports.getStores = async (req, res) => {
     })
 }
 
+const confirmOwner = (store, user) => {
+    if(!store.author.equals(user._id)){
+        throw Error('You must own a store in order to edit!')
+    }
+}
+
 exports.editStore = async(req, res) => {
     const id = req.params.id
     const store = await(Store.findById(id))
+    confirmOwner(store, req.user)
     res.render('editStore', {
         title: `Edit ${store.name}`, store 
     })
@@ -78,7 +86,7 @@ exports.updateStore = async(req, res) => {
 }
 
 exports.getStoreBySlug = async(req, res, next) => {
-    const store = await Store.findOne({ slug: req.params.slug})
+    const store = await Store.findOne({ slug: req.params.slug}).populate('author')
     if(!store){
         return next()
     }
