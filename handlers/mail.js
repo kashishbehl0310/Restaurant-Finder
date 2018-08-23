6ceeb15acac6922b8caef05450823091d79f78e8
kashishbehl0310 <kashishbehl36@gmail.com>
2018-08-24 01:28:27 +0530

    Register Page fixed

--- handlers/mail.js
+++ handlers/mail.js
@@ -1,34 +1,34 @@
-const nodemailer = require('nodemailer')
-const pug = require('pug')
-const juice = require('juice')
-const htmltotext = require('html-to-text')
-const promisify = require('es6-promisify')
+// const nodemailer = require('nodemailer')
+// const pug = require('pug')
+// const juice = require('juice')
+// const htmltotext = require('html-to-text')
+// const promisify = require('es6-promisify')
 
-const transport = nodemailer.createTransport({
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
-    auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS
-    }
-})
+// const transport = nodemailer.createTransport({
+//     host: process.env.MAIL_HOST,
+//     port: process.env.MAIL_PORT,
+//     auth: {
+//         user: process.env.MAIL_USER,
+//         pass: process.env.MAIL_PASS
+//     }
+// })
 
-const generateHtml = (filename, options = {}) => {
-    const html = pug.renderFile(`${__dirname}/../views/email/${filename}.pug`, options)
-    const inline = juice(html)
-    return inline
-}
+// const generateHtml = (filename, options = {}) => {
+//     const html = pug.renderFile(`${__dirname}/../views/email/${filename}.pug`, options)
+//     const inline = juice(html)
+//     return inline
+// }
 
-exports.send = async(options) => {
-    const html = generateHtml(options.filename, options)
-    const text = htmltotext.fromString(html)
-    const mailOptions = {
-        from: `Kashish Behl <noreply@kashish.com>`,
-        to: options.user.email,
-        subject: options.subject,
-        html,
-        text
-    }
-    const sendMail = promisify(transport.sendMail, transport)
-    return sendMail(mailOptions)
-}
+// exports.send = async(options) => {
+//     const html = generateHtml(options.filename, options)
+//     const text = htmltotext.fromString(html)
+//     const mailOptions = {
+//         from: `Kashish Behl <noreply@kashish.com>`,
+//         to: options.user.email,
+//         subject: options.subject,
+//         html,
+//         text
+//     }
+//     const sendMail = promisify(transport.sendMail, transport)
+//     return sendMail(mailOptions)
+// }
--- models/Stores.js
+++ models/Stores.js
@@ -16,28 +16,23 @@ const storeSchema = new mongoose.Schema({
   tags: [String],
   created: {
     type: Date,
-    default: Date.now,
+    default: Date.now
   },
   location: {
-   type: {
-     type: String,
-     default: 'Point'
-   },
-   coordinates:[{
-     type: Number,
-     required: 'Please supply location coordinates'
-   }],
-   address: {
-     type: String,
-     required: 'Please provide an address'
-   }
+    type: {
+      type: String,
+      default: 'Point'
+    },
+    coordinates: [{
+      type: Number,
+      required: 'You must supply coordinates!'
+    }],
+    address: {
+      type: String,
+      required: 'You must supply an address!'
+    }
   },
-  photo: String,
-  author: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'User',
-    required: 'You must supply an author'
-  }
+  photo: String
 });
 
 storeSchema.pre('save', async function(next) {
@@ -49,19 +44,19 @@ storeSchema.pre('save', async function(next) {
   // find other stores that have a slug of wes, wes-1, wes-2
   const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
   const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
-  if(storesWithSlug.length) {
+  if (storesWithSlug.length) {
     this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
   }
   next();
   // TODO make more resiliant so slugs are unique
 });
 
-storeSchema.statics.getTagsList = function(){
+storeSchema.statics.getTagsList = function() {
   return this.aggregate([
-    {$unwind: '$tags'},
-    {$group: {_id: '$tags', count: { $sum: 1 }}},
-    {$sort: { count: -1 }}
-  ])
+    { $unwind: '$tags' },
+    { $group: { _id: '$tags', count: { $sum: 1 } } },
+    { $sort: { count: -1 } }
+  ]);
 }
 
 module.exports = mongoose.model('Store', storeSchema);
