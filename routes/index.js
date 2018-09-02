58cee3c8a2e3a4aede8c37471a8c98ea0d4f917e
kashishbehl0310 <kashishbehl36@gmail.com>
2018-09-02 13:30:19 +0530

    Review System added

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController')
 const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
@@ -51,7 +52,10 @@ router.post('/account/reset/:token',
       catchErrors(authController.update) 
 )
 
-
+router.post('/reviews/:id',
+  authController.isLoggedIn,
+  catchErrors(reviewController.addReview)
+);
 router.get('/api/search', catchErrors(storeController.searchStore))
 router.get('/api/stores/near', catchErrors(storeController.mapStores))
 
